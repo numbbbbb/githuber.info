a6ab5b6cb2aee4fb443852dfd0a7bd5c2a8161a2
NatsuDawn <natsudawn233@gmail.com>
2015-02-08 18:00:19 +0800

    add rank demo
    根据设计增加rank页面

--- app/scripts/controllers/rankCtl.js
+++ app/scripts/controllers/rankCtl.js
@@ -1,7 +1,12 @@
 App.controller('rankCtl', ['$scope', '$location', function($scope, $location) {
     $('#bar-search').show(0);
     clearBDShare();
-    $scope.rank_data = [];
+    $scope.full_rank = {
+        name: "sss",
+        data: []
+    };
+    $scope.isDetail = true;
+    $scope.languages = [];
     $scope.getRankClass = function(index) {
         if (index == 0) {
             return "rank1";
@@ -14,16 +19,26 @@ App.controller('rankCtl', ['$scope', '$location', function($scope, $location) {
         }
     };
     var fake_user = {
-        username: 'username',
         name: 'XXX',
-        gravater: "https://avatars.githubusercontent.com/u/2572987?v=3",
-        follows: 322,
+        // gravater: "https://avatars.githubusercontent.com/u/2572987?v=3",
+        followers: 322,
         stars: 3222,
         repos: 222,
         location: "China",
-        language: "Javascript"
+        language: "Javascript",
+        score: 233,
+        days: 233
     }
     for (var i=0, l=10; i<l; i++) {
-        $scope.rank_data.push(fake_user);
+        $scope.full_rank.data.push(fake_user);
+    }
+    for (var i=0, l=6; i<l; i++) {
+        var temp = {};
+        temp.name = "name";
+        temp.data = [];
+        for (var j=0, l=10;j<l;j++) {
+            temp.data.push(fake_user);
+        }
+        $scope.languages.push(temp);
     }
 }]);
